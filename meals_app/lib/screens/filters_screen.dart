56addd5355ea8c3fbab4018adfd6572dc34927d2
Anton Nocum <anton.nocum@gmail.com>
2021-09-03 12:38:12 +0800

    Added switches in Filter page

--- meals_app/lib/screens/filters_screen.dart
+++ meals_app/lib/screens/filters_screen.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
 
   const FiltersScreen({Key? key}) : super(key: key);
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFree = false;
+  var _vegetarian = false;
+  var _vegan = false;
+  var _lactoseFree = false;
+
+  Widget _buildSwitchListTile(
+      {required String title,
+      required String description,
+      required bool currentValue,
+      required Function(bool) updateValue}) {
+    return SwitchListTile(
+      title: Text(
+        title,
+      ),
+      value: currentValue,
+      subtitle: Text(description),
+      onChanged: updateValue,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +38,58 @@ class FiltersScreen extends StatelessWidget {
         title: Text('Your Filters'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Filters'),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _buildSwitchListTile(
+                    title: 'Gluten-free',
+                    description: 'Only includes gluten-free meals',
+                    currentValue: _glutenFree,
+                    updateValue: (newValue) {
+                      setState(() {
+                        _glutenFree = newValue;
+                      });
+                    }),
+                _buildSwitchListTile(
+                    title: 'Vegetarian',
+                    description: 'Only includes vegetarian meals',
+                    currentValue: _vegetarian,
+                    updateValue: (newValue) {
+                      setState(() {
+                        _vegetarian = newValue;
+                      });
+                    }),
+                _buildSwitchListTile(
+                    title: 'Vegan',
+                    description: 'Only includes vegan meals',
+                    currentValue: _vegan,
+                    updateValue: (newValue) {
+                      setState(() {
+                        _vegan = newValue;
+                      });
+                    }),
+                _buildSwitchListTile(
+                    title: 'Lactose-free',
+                    description: 'Only includes lactose-free meals',
+                    currentValue: _lactoseFree,
+                    updateValue: (newValue) {
+                      setState(() {
+                        _lactoseFree = newValue;
+                      });
+                    }),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- meals_app/lib/screens/tabs_screen.dart
+++ meals_app/lib/screens/tabs_screen.dart
@@ -58,6 +58,7 @@ class _TabsScreenState extends State<TabsScreen> {
         unselectedItemColor: Colors.white,
         selectedItemColor: Theme.of(context).accentColor,
         currentIndex: _selectedPageIndex,
+        // type: BottomNavigationBarType.shifting,
       ),
     );
   }
